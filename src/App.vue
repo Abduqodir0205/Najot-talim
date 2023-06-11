0dd1de105dd7aea78e5430d519af8aeb65bd8655
Olimov001x <olimovsardorbek142@gmail.com>
2023-06-11 15:05:00 +0500

    main__3

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,31 @@
 <template>
   <div class="home__page">
-<<<<<<< HEAD
 
-    <h1>qwerty</h1>
-    <Sec2/>
+    <main>
+      <Sec2/>
+    </main>
+
   </div>
-
 </template>
 
 <script>
+
 import Sec2 from '../src/components/home-page/main/section2__main.vue'
 
-export default{
+export default {
   data() {
     return {
-      
-    }
-  },
-
-  components:{
-    Sec2,
-  }
-
-=======
-      <Header/>
-  </div>
-</template>
-
-<script>
-import Header from '../src/components/home-page/header.vue'
-
-export default{
-  data(){
-    return{
 
     }
   },
-  components:{
-    Header
+  components: {
+    Sec2
   }
->>>>>>> 58a8e31f169c585c9ad9daf465a2bd1f54be9151
+
 }
 
 </script>
 
 <style>
-<<<<<<< HEAD
-@import url(./sass/style.scss);
-
-=======
-  @import './sass/style.scss';
->>>>>>> 58a8e31f169c585c9ad9daf465a2bd1f54be9151
+@import './sass/style.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
 
 const app = createApp(App)
-
-=======
-
-
-const app = createApp(App)
-
-
->>>>>>> 58a8e31f169c585c9ad9daf465a2bd1f54be9151
 
 app.mount('#app')
