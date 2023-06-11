da6e12b367b2eba992c7b1d80f8b1b4850d84271
Olimov001x <olimovsardorbek142@gmail.com>
2023-06-11 13:29:39 +0500

    main77

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <div class="home__page"></div>
+  <div class="home__page">
+
+    <h1>qwerty</h1>
+    <Sec2/>
+  </div>
+
 </template>
 
 <script>
+import Sec2 from '../src/components/home-page/main/section2__main.vue'
+
+export default{
+  data() {
+    return {
+      
+    }
+  },
+
+  components:{
+    Sec2,
+  }
+
+}
 
 </script>
 
 <style>
+@import url(./sass/style.scss);
 
 </style>
