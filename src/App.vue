4a774c544c5f72f000da20f3c186cd28544ea3b7
Olimov001x <olimovsardorbek142@gmail.com>
2023-06-11 15:39:24 +0500

    main5

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,25 @@
 <template>
   <div class="home__page">
-<<<<<<< HEAD
-    
+    <main>
+      <MainSection2__homePage/>
+    </main>
   </div>
 </template>
 
 <script>
-export default{
-=======
-<<<<<<< HEAD
-
-    <h1>qwerty</h1>
-    <Sec2/>
-  </div>
-
-</template>
-
-<script>
-import Sec2 from '../src/components/home-page/main/section2__main.vue'
-
+import MainSection2__homePage from './components/home-page/main/section2__main.vue'
 export default{
   data() {
     return {
       
     }
   },
-
   components:{
-    Sec2,
+    MainSection2__homePage
   }
-
-=======
-      <Header/>
-  </div>
-</template>
-
-<script>
-import Header from '../src/components/home-page/header.vue'
-
-export default{
-  data(){
-    return{
-
-    }
-  },
-  components:{
-    Header
-  }
->>>>>>> 58a8e31f169c585c9ad9daf465a2bd1f54be9151
-}
->>>>>>> 748b1a8a082b8067a19d5b71e8bdc455e1523a59
-
 }
 </script>
 
 <style>
-<<<<<<< HEAD
-@import './sass/style.scss'
-=======
-<<<<<<< HEAD
-@import url(./sass/style.scss);
->>>>>>> 748b1a8a082b8067a19d5b71e8bdc455e1523a59
-
-=======
-  @import './sass/style.scss';
->>>>>>> 58a8e31f169c585c9ad9daf465a2bd1f54be9151
+@import '../css/style.min.css';
 </style>
