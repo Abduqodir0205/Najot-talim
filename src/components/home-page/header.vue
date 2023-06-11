e09c9141f71a63b4e7d30fa7c5fd9026ff9ad26d
Abdulhamidfrontedd <abdulhamidturgunov1606@gmail.com>
2023-06-11 13:39:29 +0500

    logo

--- src/components/home-page/header.vue
+++ src/components/home-page/header.vue
@@ -3,6 +3,10 @@
     <div class="header__top">
         <h1>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
 ShopNow</h1>
+        <div class="header__top__language">
+            <p>English</p>
+            <img src="" alt="">
+        </div>
     </div>
     <div class="container">
         <div class="header">
