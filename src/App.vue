1488f14a29f521b58092b9fc5d20912758fdf31f
Abdulhamidfrontedd <abdulhamidturgunov1606@gmail.com>
2023-06-11 12:14:09 +0500

    imgages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="home__page"></div>
+  <div class="home__page">
+      <Header/>
+  </div>
 </template>
 
 <script>
+import Header from '../src/components/home-page/header.vue'
+
+export default{
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    Header
+  }
+}
 
 </script>
 
 <style>
-
+  @import './sass/style.scss';
 </style>
