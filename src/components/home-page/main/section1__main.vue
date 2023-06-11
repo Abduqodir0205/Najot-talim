3dde13ce048361dd621249f26f943473aecd6e0f
Abduqodir0205 <abduqodirDeveloper@gmail.com>
2023-06-11 03:41:02 -0700

    Abduqodir

---
+++ src/components/home-page/main/section1__main.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="section1">
+    <div class="section1__top">
+      <div class="section1-top__divcha"></div>
+      <p>Today’s</p>
+    </div>
+    <div class="section1__middle">
+      <div class="section1-middle__left">
+        <h1>Most Sales</h1>
+        <div class="section1-middle__clock">
+          <div class="section-clock__days">
+            <p>Days</p>
+            <h1>03</h1>
+          </div>
+          <div class="section-clock__Hours">
+            <p>Hours</p>
+            <h1>23</h1>
+          </div>
+          <div class="section-clock__Minutes">
+            <p>Minutes</p>
+            <h1>19</h1>
+          </div>
+          <div class="section-clock__Seconds">
+            <p>Seconds</p>
+            <h1>56</h1>
+          </div>
+        </div>
+      </div>
+      <div class="section1-middle__right">
+        <div class="section1-middle__strelka">
+          <img src="../" alt="">
+        </div>
+        <div class="section1-middle__strelka">
+          <img src="" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="section1__bottom"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+@import '/Najot-talim/css/style.min.css';
+</style>
